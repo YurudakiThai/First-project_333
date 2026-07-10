02536e98d07107c131e7763e54910d3724e43079
Thanakrit_Na_Lamphun <yurudaki@gmail.com>
2026-07-10 15:28:40 +0700

    add vegeto dance and background

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,19 @@
 <template>
   <q-page class="flex flex-center">
     <div class="column items-center">
-      <img
+      <div class = "video-background-container">
+      <video autoplay muted loop playsinline class ="video-background">
+        <source src = "~@/assets/vegeto_Niudacne.mp4" type ="video/mp4">
+        Your browser doesn't support video
+      </video>
+      <div class = "video-overlay-content">
+        <img
         alt="Hello Word ALLAH LOVE YOU"
-        src="~@/assets/67-3x.gif"
+        src="~@/assets/Quasar-logo-vertical.svg"
         style="width: 400px; height: 200px"
-      />
+        />
+      </div>
+      </div>
 
       <q-btn
         class="q-mt-md"
